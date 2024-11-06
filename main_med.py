6af50c127cea96f8c165ddd553c15357507bd940
intannk <147404999+intannk@users.noreply.github.com>
2024-11-06 10:42:06 +0700

    Delete main_med.py

--- main_med.py
+++
@@ -1,7 +0,0 @@
-import medical_data_visualizer
-from unittest import main
-
-medical_data_visualizer.draw_cat_plot()
-medical_data_visualizer.draw_heat_map()
-
-main(module='test_module', exit=False)
